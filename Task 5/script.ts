201bc9dc45603a1ec3f0804a8df84f02fa6c7135
tomasbaublys <t.baublys@icloud.com>
2025-03-05 14:34:57 +0200

    Completed task 7

--- Task 5/script.ts
+++ Task 5/script.ts
@@ -7,23 +7,3 @@ Pvz.:
   "scissors", "rock" --> "Player 2 won!"
   "paper", "paper" --> "Draw!"
 -------------------------------------------------------------------------- */
-
-const rockPaperScissors = (
-  player1: 'rock' | 'paper' | 'scissors', 
-  player2: 'rock' | 'paper' | 'scissors'
-): string => {
-  if (player1 === player2) {
-    return "Draw!";
-  } else if (
-    (player1 === "rock" && player2 === "scissors") ||
-    (player1 === "paper" && player2 === "rock") ||
-    (player1 === "scissors" && player2 === "paper")
-  ) {
-    return "Player 1 won!";
-  } else {
-    return "Player 2 won!";
-  }
-};
-// console.log(rockPaperScissors("scissors", "paper"));
-// console.log(rockPaperScissors("scissors", "rock"));
-// console.log(rockPaperScissors("paper", "paper"));
--- Task 7/script.js
+++ Task 7/script.js
@@ -1,56 +1,34 @@
+"use strict";
 /* ------------------------------ TASK 7 -----------------------------------
-Parašykite TS kodą, vartotojui atėjus į tinklapį kreipsis į NBA.json failą ir iš jo atvaizduos visas NBA komandas ir jų žaidėjus. 
-Kiekviena komanda turės savo atvaizdavimo "kortelę", kurioje bus 
+Parašykite TS kodą, vartotojui atėjus į tinklapį kreipsis į NBA.json failą ir iš jo atvaizduos visas NBA komandas ir jų žaidėjus.
+Kiekviena komanda turės savo atvaizdavimo "kortelę", kurioje bus
 nurodomas komandos pavadinimas ir papildomose "mini kortelėse" išvardinti žaidėjai su vardais, pavardėmis ir nuoroda į daugiau informacijos apie juos.
 
 Pastaba: Informacija apie komandas bei žaidėjus turi turėti bent minimalų stilių;
 -------------------------------------------------------------------------- */
-
-type Player = {
-    firstName: string;
-    lastName: string;
-    googleSearch: string;
-};
-
-type TeamData = {
-    id: number;
-    name: string;
-    players: Player[];
-};
-
-type NBA_Data = {
-    teams: TeamData[];
-};
-
-const ENDPOINT: string = 'NBA.json';
-
+const ENDPOINT = 'NBA.json';
 fetch(ENDPOINT)
-.then(res => res.json())
-.then((data: NBA_Data) => {
-    const output: HTMLDivElement | null = document.querySelector('#output');
+    .then(res => res.json())
+    .then((data) => {
+    const output = document.querySelector('#output');
     if (!output) {
         throw new Error(`The element does not exist in the HTML file`);
     }
-    data.teams.forEach((team: TeamData) => {
-        const teamContainer: HTMLDivElement = document.createElement('div');
+    data.teams.forEach((team) => {
+        const teamContainer = document.createElement('div');
         teamContainer.classList.add('team-container');
-
-        const teamTitle: HTMLHeadingElement = document.createElement('h3');
+        const teamTitle = document.createElement('h3');
         teamTitle.textContent = team.name;
         teamContainer.appendChild(teamTitle);
-
-        team.players.forEach((player: Player) => {
-            const playerContainer: HTMLDivElement = document.createElement('div');
+        team.players.forEach((player) => {
+            const playerContainer = document.createElement('div');
             playerContainer.classList.add('player-container');
-
-            const playerName: HTMLSpanElement = document.createElement('span');
+            const playerName = document.createElement('span');
             playerName.textContent = player.firstName.concat(' ', player.lastName);
-
-            const playerInfoLink: HTMLAnchorElement = document.createElement('a');
+            const playerInfoLink = document.createElement('a');
             playerInfoLink.textContent = 'More info';
             playerInfoLink.setAttribute('href', player.googleSearch);
             playerInfoLink.setAttribute('target', '_blank');
-
             playerContainer.append(playerName, playerInfoLink);
             teamContainer.appendChild(playerContainer);
         });
