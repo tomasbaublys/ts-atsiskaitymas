5e4ba9cbe7a865d96d3105a6799c07f9cb8b0244
tomasbaublys <t.baublys@icloud.com>
2025-03-05 14:36:45 +0200

    Restored tasks 4 / 5 / 6

--- Task 4/script.ts
+++ Task 4/script.ts
@@ -6,3 +6,15 @@ Pvz.:
   "kebabas" --> 1010101
   "a"       --> 1
 -------------------------------------------------------------------------- */
+
+function textToNumber(text: string): number {
+  if (text.length === 0) return 0;
+  let num = 1;
+  for (let i = 1; i < text.length; i++) {
+    num = num * 10 + (i % 2 === 0 ? 1 : 0);
+  }
+  return num;
+}
+// console.log(textToNumber("labas"));   // 10101
+// console.log(textToNumber("kebabas")); // 1010101
+// console.log(textToNumber("a"));       // 1
